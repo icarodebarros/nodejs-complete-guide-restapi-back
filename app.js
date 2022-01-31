128b6c35f88bdb999d6b4ca04e85dccbdab268fc
Icaro de Barros <icarodebarros@gmail.com>
2022-01-31 12:41:47 -0300

    Signup, login with jwt

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -48,12 +49,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => { // Error Handler
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 
   /**
     Em códigos síncronos (fora de promises) pode-se lançar erros (throw new Error...) que garantidamente será capturado pelo próximo error handler;
