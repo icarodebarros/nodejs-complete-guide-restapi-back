77f72f212db58d259cb84a9ccf0dfbe6a37c8a3d
Icaro de Barros <icarodebarros@gmail.com>
2023-01-20 12:51:33 -0300

    Fixing images names problem on windows

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, new Date().toISOString().replaceAll(':', '_') + '-' + file.originalname);
   }
 });
 
